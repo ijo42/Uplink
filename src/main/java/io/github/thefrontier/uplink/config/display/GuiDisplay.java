ddfd9294efc6e65013c1535a033131d628f9fbe6
ijo42 <admin@ijo42.ru>
2019-08-25 23:42:00 +0500

    Cutted unused

--- src/main/java/io/github/thefrontier/uplink/config/display/GuiDisplay.java
+++
@@ -1,9 +0,0 @@
-package io.github.thefrontier.uplink.config.display;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class GuiDisplay {
-
-    public Map<String, String> classNameToInfo = new HashMap<>();
-}
